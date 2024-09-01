265240dcb0420d4b395a1fbe47302e278561dd71
ACE-Hamza <hrktv7hub@gmail.com>
2024-09-01 15:48:16 +0530

    React-Django Setup

--- interactive_fiction/interactive_fiction/settings.py
+++ interactive_fiction/interactive_fiction/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework'
 ]
 
 # AUTH_USER_MODEL = 'stories.User'
@@ -50,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React dev server
 ]
 
 ROOT_URLCONF = 'interactive_fiction.urls'
--- interactive_fiction/stories/views.py
+++ interactive_fiction/stories/views.py
@@ -1,55 +1,68 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from .models import Story, Chapter, Choice, UserProgress
 from django.contrib.auth.models import User
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import StorySerializer # , ChapterSerializer, ChoiceSerializer, UserProgressSerializer
 
-class StoryListView(ListView):
-    model = Story
-    template_name = 'stories/story_list.html'
-    context_object_name = 'stories'
+# API View for listing all stories
+class StoryListView(APIView):
+    def get(self, request):
+        stories = Story.objects.all()
+        serializer = StorySerializer(stories, many=True)
+        return Response(serializer.data)
 
-class StoryDetailView(DetailView):
-    model = Story
-    template_name = 'stories/story_detail.html'
-    context_object_name = 'story'
+# API View for getting the details of a specific story
+class StoryDetailView(APIView):
+    def get(self, request, pk):
+        story = get_object_or_404(Story, pk=pk)
+        serializer = StorySerializer(story)
+        return Response(serializer.data)
 
-class ChapterDetailView(DetailView):
-    model = Chapter
-    template_name = 'stories/chapter_detail.html'
-    context_object_name = 'chapter'
+# API View for getting the details of a specific chapter
+# class ChapterDetailView(APIView):
+#     @method_decorator(login_required)
+#     def post(self, request, pk):
+#         chapter = get_object_or_404(Chapter, pk=pk)
+#         choice_id = request.data.get('choice_id')
+#         choice = get_object_or_404(Choice, id=choice_id)
 
-    def post(self, request, *args, **kwargs):
-        chapter = self.get_object()
-        choice_id = request.POST.get('choice_id')
-        choice = get_object_or_404(Choice, id=choice_id)
-        
-        # Update user progress
-        progress, created = UserProgress.objects.get_or_create(
-            user=request.user, story=chapter.story,
-            defaults={'current_chapter': choice.next_chapter}
-        )
-        if not created:
-            progress.current_chapter = choice.next_chapter
-            progress.save()
+#         # Update user progress
+#         progress, created = UserProgress.objects.get_or_create(
+#             user=request.user,
+#             story=chapter.story,
+#             defaults={'current_chapter': choice.next_chapter}
+#         )
+#         if not created:
+#             progress.current_chapter = choice.next_chapter
+#             progress.save()
 
-        return redirect('chapter_detail', pk=choice.next_chapter.pk)
+#         serializer = ChapterSerializer(choice.next_chapter)
+#         return Response(serializer.data)
 
-class CreateStoryView(CreateView):
-    model = Story
-    fields = ['title', 'description']
-    template_name = 'stories/story_form.html'
+#     def get(self, request, pk):
+#         chapter = get_object_or_404(Chapter, pk=pk)
+#         serializer = ChapterSerializer(chapter)
+#         return Response(serializer.data)
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
+# API View for creating a new story
+class CreateStoryView(APIView):
+    @method_decorator(login_required)
+    def post(self, request):
+        serializer = StorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class UserProgressView(DetailView):
-    model = User
-    template_name = 'stories/user_progress.html'
-    context_object_name = 'user'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['progress'] = UserProgress.objects.filter(user=self.object)
-        return context
+# API View for retrieving user progress
+# class UserProgressView(APIView):
+#     @method_decorator(login_required)
+#     def get(self, request, user_id):
+#         user = get_object_or_404(User, pk=user_id)
+#         progress = UserProgress.objects.filter(user=user)
+#         serializer = UserProgressSerializer(progress, many=True)
+#         return Response(serializer.data)
